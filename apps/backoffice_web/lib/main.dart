d9d3aad38a9d29d28ac5fcd5787c95c502f91f42
Gil Marcel Gabilheri <gmarcel@msn.com>
2026-04-04 22:38:39 -0400

    adicionado npm

--- apps/backoffice_web/lib/main.dart
+++ apps/backoffice_web/lib/main.dart
@@ -19,7 +19,7 @@ void main() async {
   // Serverpod Client Setup
   const serverUrlFromEnv = String.fromEnvironment('SERVER_URL');
   final serverUrl = serverUrlFromEnv.isEmpty
-      ? 'http://192.168.30.190/'
+      ? 'http://192.168.30.190:8090/'
       : serverUrlFromEnv;
 
   client = Client(
